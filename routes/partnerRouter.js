319b33bc6f1dffd048bb4d07c0572a774081fe93
Kris Hvattum <klhvattum@live.com>
2023-02-25 13:44:04 -0800

    Week 2 Workshop complete

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -37,25 +37,38 @@ partnerRouter.route('/')
     });
 
 partnerRouter.route('/:partnerId')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
-    })
-    .get((req, res) => {
-        res.end(`Will send ${req.params.partnerId} to you`)
-    })
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+        .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+        })
+        .catch(err => next(err));
+})
     .post((req, res) => {
         res.statusCode = 403;
         res.end(`POST operation not supported individual /partners/${req.params.partnerId}`)
     })
-    .put((req, res) => {
-        res.write(`Updating the partner: ${req.params.partnerId}`)
-        res.end(`Will update the partner: ${req.body.name}
-    with description: ${req.body.description}`)
+    .put((req, res, next) => {
+        Partner.findByIdAndUpdate(req.params.partnerId, {
+            $set: req.body
+        }, { new: true })
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
     })
-    .delete((req, res) => {
-        res.end(`Deleting partner: ${req.params.partnerId}`)
+    .delete((req, res, next) => {
+        Partner.findByIdAndDelete(req.params.partnerId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
     })
 
 module.exports = partnerRouter; 
